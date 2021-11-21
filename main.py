ecaaa5f6e32c70c1265b09f461331cdc39ec013f
Nitin Faye <47500844+nitinfaye@users.noreply.github.com>
2021-11-21 21:05:19 +0530

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import requests
-url = 'http://chatbot_heroku.com/webhooks/rest/webhook' ##change rasablog with your app name
-myobj = {
-"message": "hi",
-"sender": "Nitin",
-}
-x = requests.post(url, json = myobj)
-print(x.text)
